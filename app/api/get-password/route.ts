b2abc0937dfea27e287e142149ea865b894f9a48
Marco BARNIG <mbarnig@pt.lu>
2024-08-30 22:47:30 +0200

    Update route.ts

--- app/api/get-password/route.ts
+++ app/api/get-password/route.ts
@@ -2,6 +2,7 @@ SESSION_START_PASSWORD = process.env.SESSION_START_PASSWORD;
 
 export async function () {
   try {
-    if (!HEYGEN_API_KEY) {
-      console.error("API key is missing from .env");
+    if (SESSION_START_PASSWORD) {
+      return SESSION_START_PASSWORD
     }
+    console.error("API key is missing from .env");
