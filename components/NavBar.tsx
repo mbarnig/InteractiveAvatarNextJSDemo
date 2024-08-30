08d3afd1fda096d2ea92ca4351618bb7d4ac6158
Marco BARNIG <mbarnig@pt.lu>
2024-08-30 17:34:28 +0200

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -14,15 +14,13 @@ export default function NavBar() {
   return (
     <Navbar className="w-full">
       <NavbarBrand>
-        <Link isExternal aria-label="HeyGen" href="https://app.heygen.com/">
-          <HeyGenLogo />
-        </Link>
         <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text ml-4">
           <p className="text-xl font-semibold text-transparent">
             My HeyGen Interactive Avatar SDK NextJS Demo
           </p>
         </div>
       </NavbarBrand>
+      
       <NavbarContent justify="center">
           <Link
             isExternal
